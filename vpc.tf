3a5e6aaa1c779b72c05c895da32136bf69adc8a0
lucienlgl <884078898@qq.com>
2026-06-09 15:34:00 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ variable "region" {
 
 variable "endpoint" {
   type        = string
-  description = "自定义API endpoint"
+  description = "自定义API endpoints"
   default     = "open.volcengineapi.com"
 }
 
