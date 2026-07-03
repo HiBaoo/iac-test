eb13e066da66cb5db790f3053df6f44a7d913993
wangshuang.ws1 <wangshuang.ws1@bytedance.com>
2026-07-03 14:34:28 +0800

    增加bmq

--- bmq_instance/volcengine/variables.tf
+++ bmq_instance/volcengine/variables.tf
@@ -1,19 +1,19 @@
 variable "project_name" {
   type    = string
 }
-#
-# variable "tags" {
-#   type = list(object({
-#     key   = string
-#     type  = string
-#     value = string
-#   }))
-#   default = [{
-#     key   = "env"
-#     type  = "CUSTOM"
-#     value = "test"
-#   }]
-# }
+
+variable "tags" {
+  type = list(object({
+    key   = string
+    type  = string
+    value = string
+  }))
+  default = [{
+    key   = "env"
+    type  = "CUSTOM"
+    value = "test"
+  }]
+}
 
 variable "instance_name" {
   type = string
